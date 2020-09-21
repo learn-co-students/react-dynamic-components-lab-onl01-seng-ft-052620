f421de2bb9894ab16419440c1ec4223d4b889b1b
shazy89 <erdo.shazy123@gmail.com>
2020-09-21 13:58:19 -0400

    Done.

---
+++ build/precache-manifest.a1abf5bb1a9cd4d85d7a4eecf6948b88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccd6405130d4b3d53937",
+    "url": "/static/css/main.573b5e16.chunk.css"
+  },
+  {
+    "revision": "ccd6405130d4b3d53937",
+    "url": "/static/js/main.3defb1e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "b0ef91adfcd25174d4967b31f412e617",
+    "url": "/index.html"
+  }
+];
