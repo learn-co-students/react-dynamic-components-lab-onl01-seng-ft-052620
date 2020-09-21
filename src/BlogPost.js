0408701ee38fd82d052392436738808e03446144
naechrist <729jenae.christensen@gmail.com>
2020-09-21 14:56:27 -0400

    Done.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,33 +1,3 @@
-// import React, { Component } from 'react';
-// import Comment from './Comment';
-
-// export default class BlogPost extends Component {
-//   render() {
-    
-//     const comments = [
-      
-//       "I am no longer accepting the things I cannot change. I am changing the things I cannot accept. - Angela Davis",
-//       "If you don't understand, ask questions. If you're uncomfortable about asking questions, say you are uncomfortable about asking questions and then ask anyway. It's easy to tell when a question is coming from a good place. Then listen some more. Sometimes people just want to feel heard. Here's to possibilities of friendship and connection and understanding. - Chimamanda Ngozi Adichie"
-//     ]
-    
-//     return (
-//       <div id="blog-post" className="wrapper">
-//         Alright internet trolls<br/>
-//         Come at me<br/>
-//         I have been preparing<br/>
-//         My meme fortress is built<br/>
-//         I won't let you tilt me<br/>
-//         You only grew accustomed to the salt<br/>
-//         I was born in it<br/>
-
-//         <Comment commentText={comments[0]} />
-//         <Comment commentText={comments[0]} />
-//         <Comment commentText={comments[0]} />
-        
-//       </div>
-//     );
-//   }
-// }
 import React, { Component } from 'react';
 import Comment from './Comment.js'
 
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,23 +1,3 @@
-// import React, { Component } from 'react';
-
-// export default class ColorBox extends Component {
-
-//   state = {
-//     todos: [
-      
-//     ]
-//   }
-
-//   render() {
-//     return (
-//       <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-//         {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1}/> : 1}
-//       </div>
-//     )
-//   }
-
-// }
-
 import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
