902299e7a8831cd3d41d4b86a3a822419185e7ed
Luca <lucakanyo@protonmail.com>
2020-09-30 16:20:27 -0400

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -8,14 +8,6 @@ export default class ColorBox extends Component {
     ]
   }
 
-  // render() { 
-  //   return (
-  //     <div className="color-box" style={{opacity: this.props.opacity}}>
-  //       {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1} /> : ''}
-  //     </div>
-  //   )
-  // }
-
   render(){
     return this.props.opacity >= 0.2 ? (
       <div className="color-box" style={{opacity: this.props.opacity}}>
@@ -25,11 +17,4 @@ export default class ColorBox extends Component {
     }
 
 }
-
-// render() {
-//   const newValue = this.props.value * 2;
-//   return this.props.value > 100 ? null : ( <div>
-//       <Example value={newValue} />
-//     </div>)
-// }
 
